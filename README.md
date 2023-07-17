68f898820cca01868c139e88619594a4bc44116a
Simon Oh <dev.ohss@gmail.com>
2023-07-17 10:13:03 -0400

    #17.5 Form Login

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
-# Nuber Eats
+## Nuber Eats FrontEnd
 
+
+
+
+
+# 17.5 Form Login
+Ring Width
+box shadow가 있는 outline을 만들기 위한 유틸리티입니다.
+https://tailwindcss.com/docs/ring-width
+
+useForm()에서 errors대신 formState: { errors }로 가져오시면 됩니다.
+
+PostCSS Language Support
+위 vscode extension설치하면
+@tailwind, @apply에 syntax error가 없어집니다
+
+
+# 17.4 Form Design
+TailwindCSS 클래스 검색
+TailwindCSS 클래스들을 검색하고 찾아볼 수 있는 사이트입니다.
+https://tailwind.build/classes
+https://shuffle.dev/tailwind/classes
+
+클래스 삭제 후, 자동완성 다시 켜기(Mac)
+스페이스바 또는 option+esc
+
+
+
+## Nuber Eats BackEnd
 #16.1 TailwindCSS part One
 https://tailwindcss.com/docs/installation
 npm install -D tailwindcss postcss autoprefixer
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,70 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+interface ILoginForm {
+  email?: string;
+  password?: string;
+}
 
 export const Login = () => {
-  return <span>Login</span>;
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILoginForm>();
+  const onSubmit = () => {
+    console.log("gsddasdas");
+    console.log(getValues());
+  };
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-800">
+      <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
+        <h3 className="text-2xl text-gray-800">Log In</h3>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="grid gap-3 mt-5 px-5"
+        >
+          <input
+            {...register("email", {
+              required: "This is required",
+              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+            })}
+            name="email"
+            required
+            type="email"
+            placeholder="Email"
+            className="input"
+          />
+          {errors.email?.message && (
+            <span className="font-medium text-red-500">
+              {errors.email?.message}
+            </span>
+          )}
+          <input
+            {...register("password", {
+              required: "Password is required",
+              minLength: 10,
+            })}
+            required
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="input"
+          />
+          {errors.password?.message && (
+            <span className="font-medium text-red-500">
+              {errors.password?.message}
+            </span>
+          )}
+          {errors.password?.type === "minLength" && (
+            <span className="font-medium text-red-500">
+              Password must be more than 10 chars.
+            </span>
+          )}
+          <button className="mt-3 btn">Log In</button>
+        </form>
+      </div>
+    </div>
+  );
 };
