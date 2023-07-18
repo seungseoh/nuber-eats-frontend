24630d78b58e945afe4a1af2fd558cc76800cb05
Simon Oh <dev.ohss@gmail.com>
2023-07-18 16:45:16 -0400

    #18.3 Edit Profile part Two

--- src/pages/user/edit-profile.tsx
+++ src/pages/user/edit-profile.tsx
@@ -1,16 +1,106 @@
 import React from "react";
+import { gql, useMutation } from "@apollo/client";
 import { Button } from "../../components/button";
 import { useMe } from "../../hooks/useMe";
+import {
+  EditProfileMutation,
+  EditProfileMutationVariables,
+} from "../../__types__/graphql";
+import { useForm } from "react-hook-form";
+import { EMAIL_PATTERN } from "../../constants";
+
+const EDIT_PROFILE_MUTATION = gql`
+  mutation editProfile($input: EditProfileInput!) {
+    editProfile(input: $input) {
+      ok
+      error
+    }
+  }
+`;
+
+interface IFormProps {
+  email?: string;
+  password?: string;
+}
 
 export const EditProfile = () => {
   const { data: userData } = useMe();
+  const onCompleted = (data: EditProfileMutation) => {
+    const {
+      editProfile: { ok },
+    } = data;
+    if (ok) {
+      // update the cache
+    }
+  };
+  const [editProfile, { loading }] = useMutation<
+    EditProfileMutation,
+    EditProfileMutationVariables
+  >(EDIT_PROFILE_MUTATION, {
+    onCompleted,
+  });
+  const { register, handleSubmit, getValues, formState } = useForm<IFormProps>({
+    mode: "onChange",
+    defaultValues: {
+      email: userData?.me.email,
+    },
+  });
+  const onSubmit = () => {
+    const { email, password } = getValues();
+    // password가 빈 string과 같지 않으면 password가 포함된 object를 return하고 object를 풀라
+    editProfile({
+      variables: {
+        input: {
+          email,
+          ...(password !== "" && { password }),
+        },
+      },
+    });
+  };
   return (
     <div className="mt-52 flex flex-col justify-center items-center">
       <h4 className="font-semibold text-2xl mb-3">Edit Profile</h4>
-      <form className="grid max-w-screen-sm gap-3 mt-5 w-full mb-5">
-        <input className="input" type="email" placeholder="Email" />
-        <input className="input" type="password" placeholder="Password" />
-        <Button loading={false} canClick={true} actionText="Save Profile" />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid max-w-screen-sm gap-3 mt-5 w-full mb-5"
+      >
+        <input
+          {...register("email", {
+            required: {
+              value: true,
+              message: "Email is required",
+            },
+            pattern: {
+              value: EMAIL_PATTERN,
+              message: "이메일 형식으로 입력해야 합니다.",
+            },
+          })}
+          name="email"
+          type="email"
+          placeholder="Email"
+          className="input"
+        />
+        <input
+          {...register("password", {
+            required: {
+              value: true,
+              message: "Password is required",
+            },
+            minLength: {
+              value: 3,
+              message: "Password must be more than 3 chars.",
+            },
+          })}
+          name="password"
+          type="password"
+          placeholder="Password"
+          className="input"
+        />
+        <Button
+          loading={loading}
+          canClick={formState.isValid}
+          actionText="Save Profile"
+        />
       </form>
     </div>
   );
